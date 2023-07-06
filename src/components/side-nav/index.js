759ca46dbeaeecc668452ba3bf879adcba93f406
Payal Sasmal <sasmalpayal@gmail.com>
2023-07-06 22:03:05 +0530

    components setups are done

---
+++ src/components/side-nav/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./side-nav.css";
+
+const SideNav = () => {
+  return (
+    <div className='side-nav'>
+        <h1>E-Com</h1>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default SideNav;
